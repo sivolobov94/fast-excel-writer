bf4706e00981c0341424bfe1a9d11bb05648ec8b
d.sivolobov <d.sivolobov@plein.com>
2023-12-19 14:56:47 +0300

    remove Intl ext from dependencies

--- src/FastExcelWriter/Excel.php
+++ src/FastExcelWriter/Excel.php
@@ -283,10 +283,10 @@ class Excel implements InterfaceBookWriter
     public function setDefaultLocale()
     {
         $this->setLocale('en');
-        $locale = \Locale::getDefault();
+        /*$locale = \Locale::getDefault();
         if ($locale) {
             $this->setLocale($locale);
-        }
+        }*/
     }
 
     /**
